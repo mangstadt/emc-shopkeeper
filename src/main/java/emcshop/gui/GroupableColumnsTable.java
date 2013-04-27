2f779b0d2d31e20f66ed65506faaeaf7bc8d2837
Michael Angstadt <mike.angstadt@gmail.com>
2013-04-27 12:32:33 -0400

    Separated the out the quantity and rupees into their own columns.

---
+++ src/main/java/emcshop/gui/GroupableColumnsTable.java
@@ -0,0 +1,327 @@
+package emcshop.gui;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.plaf.basic.BasicTableHeaderUI;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+
+//@formatter:off
+/**
+ * A table that contains groupable columns.
+ * @author Nobuo Tamemasa
+ * @author Michael Angstadt
+ * @see <a href="http://www.java2s.com/Code/Java/Swing-Components/GroupableGroupHeaderExample.htm">http://www.java2s.com/Code/Java/Swing-Components/GroupableGroupHeaderExample.htm</a>
+ */
+//@formatter:on
+@SuppressWarnings("serial")
+public abstract class GroupableColumnsTable extends JTable {
+	/**
+	 * Sets the column groups. This must be called by the child class in order
+	 * to define the column groupings.
+	 * @param columnGroups the column groups
+	 */
+	protected void setColumnGroups(List<ColumnGroup> columnGroups) {
+		GroupableTableHeader header = (GroupableTableHeader) getTableHeader();
+		for (ColumnGroup columnGroup : columnGroups) {
+			header.addColumnGroup(columnGroup);
+		}
+	}
+
+	@Override
+	protected JTableHeader createDefaultTableHeader() {
+		return new GroupableTableHeader(columnModel);
+	}
+
+	/**
+	 * A column group.
+	 * @author Nobuo Tamemasa
+	 * @author Michael Angstadt
+	 */
+	public static class ColumnGroup {
+		private TableCellRenderer renderer;
+		private List<Object> v = new ArrayList<Object>();
+		private String text;
+		private int margin = 0;
+
+		public ColumnGroup(String text) {
+			this(null, text);
+		}
+
+		public ColumnGroup(TableCellRenderer renderer, String text) {
+			if (renderer == null) {
+				this.renderer = new DefaultTableCellRenderer() {
+					public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+						JTableHeader header = table.getTableHeader();
+						if (header != null) {
+							setForeground(header.getForeground());
+							setBackground(header.getBackground());
+							setFont(header.getFont());
+						}
+						setHorizontalAlignment(JLabel.CENTER);
+						setText((value == null) ? "" : value.toString());
+						setBorder(UIManager.getBorder("TableHeader.cellBorder"));
+						return this;
+					}
+				};
+			} else {
+				this.renderer = renderer;
+			}
+			this.text = text;
+		}
+
+		/**
+		 * @param obj TableColumn or ColumnGroup
+		 */
+		public void add(Object obj) {
+			if (obj == null) {
+				return;
+			}
+			v.add(obj);
+		}
+
+		/**
+		 * @param c TableColumn
+		 * @param v ColumnGroups
+		 */
+		public List<ColumnGroup> getColumnGroups(TableColumn c, List<ColumnGroup> g) {
+			g.add(this);
+			if (v.contains(c)) return g;
+			for (Object obj : v) {
+				if (obj instanceof ColumnGroup) {
+					List<ColumnGroup> groups = ((ColumnGroup) obj).getColumnGroups(c, new ArrayList<ColumnGroup>(g));
+					if (groups != null) return groups;
+				}
+			}
+			return null;
+		}
+
+		public TableCellRenderer getHeaderRenderer() {
+			return renderer;
+		}
+
+		public void setHeaderRenderer(TableCellRenderer renderer) {
+			if (renderer != null) {
+				this.renderer = renderer;
+			}
+		}
+
+		public Object getHeaderValue() {
+			return text;
+		}
+
+		public Dimension getSize(JTable table) {
+			Component comp = renderer.getTableCellRendererComponent(table, getHeaderValue(), false, false, -1, -1);
+			int height = comp.getPreferredSize().height;
+			int width = 0;
+			for (Object obj : v) {
+				if (obj instanceof TableColumn) {
+					TableColumn aColumn = (TableColumn) obj;
+					width += aColumn.getWidth();
+					width += margin;
+				} else {
+					width += ((ColumnGroup) obj).getSize(table).width;
+				}
+			}
+			return new Dimension(width, height);
+		}
+
+		public void setColumnMargin(int margin) {
+			this.margin = margin;
+			for (Object obj : v) {
+				if (obj instanceof ColumnGroup) {
+					((ColumnGroup) obj).setColumnMargin(margin);
+				}
+			}
+		}
+	}
+
+	private static class GroupableTableHeader extends JTableHeader {
+		protected List<ColumnGroup> columnGroups = null;
+
+		public GroupableTableHeader(TableColumnModel model) {
+			super(model);
+			setUI(new GroupableTableHeaderUI());
+			setReorderingAllowed(false);
+		}
+
+		@Override
+		public void updateUI() {
+			setUI(new GroupableTableHeaderUI());
+		}
+
+		@Override
+		public void setReorderingAllowed(boolean b) {
+			reorderingAllowed = false;
+		}
+
+		public void addColumnGroup(ColumnGroup g) {
+			if (columnGroups == null) {
+				columnGroups = new ArrayList<ColumnGroup>();
+			}
+			columnGroups.add(g);
+		}
+
+		public List<ColumnGroup> getColumnGroups(TableColumn col) {
+			if (columnGroups == null) return null;
+			for (ColumnGroup cGroup : columnGroups) {
+				List<ColumnGroup> v_ret = cGroup.getColumnGroups(col, new ArrayList<ColumnGroup>());
+				if (v_ret != null) {
+					return v_ret;
+				}
+			}
+			return null;
+		}
+
+		public void setColumnMargin() {
+			if (columnGroups == null) return;
+			int columnMargin = getColumnModel().getColumnMargin();
+			for (ColumnGroup cGroup : columnGroups) {
+				cGroup.setColumnMargin(columnMargin);
+			}
+		}
+
+	}
+
+	private static class GroupableTableHeaderUI extends BasicTableHeaderUI {
+		@Override
+		public void paint(Graphics g, JComponent c) {
+			Rectangle clipBounds = g.getClipBounds();
+			if (header.getColumnModel() == null) return;
+			((GroupableTableHeader) header).setColumnMargin();
+			int column = 0;
+			Dimension size = header.getSize();
+			Rectangle cellRect = new Rectangle(0, 0, size.width, size.height);
+			Map<ColumnGroup, Rectangle> h = new HashMap<ColumnGroup, Rectangle>();
+			int columnMargin = header.getColumnModel().getColumnMargin();
+
+			Enumeration<TableColumn> enumeration = header.getColumnModel().getColumns();
+			while (enumeration.hasMoreElements()) {
+				cellRect.height = size.height;
+				cellRect.y = 0;
+				TableColumn aColumn = enumeration.nextElement();
+				List<ColumnGroup> cGroups = ((GroupableTableHeader) header).getColumnGroups(aColumn);
+				if (cGroups != null) {
+					int groupHeight = 0;
+					for (ColumnGroup cGroup : cGroups) {
+						Rectangle groupRect = h.get(cGroup);
+						if (groupRect == null) {
+							groupRect = new Rectangle(cellRect);
+							Dimension d = cGroup.getSize(header.getTable());
+							groupRect.width = d.width;
+							groupRect.height = d.height;
+							h.put(cGroup, groupRect);
+						}
+						paintCell(g, groupRect, cGroup);
+						groupHeight += groupRect.height;
+						cellRect.height = size.height - groupHeight;
+						cellRect.y = groupHeight;
+					}
+				}
+				cellRect.width = aColumn.getWidth() + columnMargin;
+				if (cellRect.intersects(clipBounds)) {
+					paintCell(g, cellRect, column);
+				}
+				cellRect.x += cellRect.width;
+				column++;
+			}
+		}
+
+		private void paintCell(Graphics g, Rectangle cellRect, int columnIndex) {
+			TableColumn aColumn = header.getColumnModel().getColumn(columnIndex);
+			TableCellRenderer renderer = aColumn.getHeaderRenderer();
+			//revised by Java2s.com
+			renderer = new DefaultTableCellRenderer() {
+				public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+					JLabel header = new JLabel();
+					header.setForeground(table.getTableHeader().getForeground());
+					header.setBackground(table.getTableHeader().getBackground());
+					header.setFont(table.getTableHeader().getFont());
+
+					header.setHorizontalAlignment(JLabel.CENTER);
+					header.setText(value.toString());
+					header.setBorder(UIManager.getBorder("TableHeader.cellBorder"));
+					return header;
+				}
+
+			};
+			Component c = renderer.getTableCellRendererComponent(header.getTable(), aColumn.getHeaderValue(), false, false, -1, columnIndex);
+
+			c.setBackground(UIManager.getColor("control"));
+
+			rendererPane.add(c);
+			rendererPane.paintComponent(g, c, header, cellRect.x, cellRect.y, cellRect.width, cellRect.height, true);
+		}
+
+		private void paintCell(Graphics g, Rectangle cellRect, ColumnGroup cGroup) {
+			TableCellRenderer renderer = cGroup.getHeaderRenderer();
+			//revised by Java2s.com
+			// if(renderer == null){
+			//      return ;
+			//    }
+
+			Component component = renderer.getTableCellRendererComponent(header.getTable(), cGroup.getHeaderValue(), false, false, -1, -1);
+			rendererPane.add(component);
+			rendererPane.paintComponent(g, component, header, cellRect.x, cellRect.y, cellRect.width, cellRect.height, true);
+		}
+
+		private int getHeaderHeight() {
+			int height = 0;
+			TableColumnModel columnModel = header.getColumnModel();
+			for (int column = 0; column < columnModel.getColumnCount(); column++) {
+				TableColumn aColumn = columnModel.getColumn(column);
+				TableCellRenderer renderer = aColumn.getHeaderRenderer();
+				//revised by Java2s.com
+				if (renderer == null) {
+					return 60;
+				}
+
+				Component comp = renderer.getTableCellRendererComponent(header.getTable(), aColumn.getHeaderValue(), false, false, -1, column);
+				int cHeight = comp.getPreferredSize().height;
+				List<ColumnGroup> e = ((GroupableTableHeader) header).getColumnGroups(aColumn);
+				if (e != null) {
+					for (ColumnGroup cGroup : e) {
+						cHeight += cGroup.getSize(header.getTable()).height;
+					}
+				}
+				height = Math.max(height, cHeight);
+			}
+			return height;
+		}
+
+		private Dimension createHeaderSize(long width) {
+			TableColumnModel columnModel = header.getColumnModel();
+			width += columnModel.getColumnMargin() * columnModel.getColumnCount();
+			if (width > Integer.MAX_VALUE) {
+				width = Integer.MAX_VALUE;
+			}
+			return new Dimension((int) width, getHeaderHeight());
+		}
+
+		@Override
+		public Dimension getPreferredSize(JComponent c) {
+			long width = 0;
+			Enumeration<TableColumn> enumeration = header.getColumnModel().getColumns();
+			while (enumeration.hasMoreElements()) {
+				TableColumn aColumn = enumeration.nextElement();
+				width = width + aColumn.getPreferredWidth();
+			}
+			return createHeaderSize(width);
+		}
+	}
+}
--- src/main/java/emcshop/gui/ItemsTable.java
+++ src/main/java/emcshop/gui/ItemsTable.java
@@ -3,6 +3,7 @@ package emcshop.gui;
 import java.awt.Color;
 import java.awt.Component;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -20,17 +21,23 @@ import emcshop.gui.images.ImageManager;
  * @author Michael Angstadt
  */
 @SuppressWarnings("serial")
-public class ItemsTable extends JTable {
+public class ItemsTable extends GroupableColumnsTable {
 	private static final int COL_ITEMNAME = 0;
-	private static final int COL_SOLD = 1;
-	private static final int COL_BOUGHT = 2;
-	private static final int COL_NET = 3;
-	private static final String[] columnNames = new String[4];
+	private static final int COL_SOLD_QTY = 1;
+	private static final int COL_SOLD_AMT = 2;
+	private static final int COL_BOUGHT_QTY = 3;
+	private static final int COL_BOUGHT_AMT = 4;
+	private static final int COL_NET_QTY = 5;
+	private static final int COL_NET_AMT = 6;
+	private static final String[] columnNames = new String[7];
 	{
 		columnNames[COL_ITEMNAME] = "Item Name";
-		columnNames[COL_SOLD] = "Sold";
-		columnNames[COL_BOUGHT] = "Bought";
-		columnNames[COL_NET] = "Net";
+		columnNames[COL_SOLD_QTY] = "Qty";
+		columnNames[COL_SOLD_AMT] = "Rupees";
+		columnNames[COL_BOUGHT_QTY] = "Qty";
+		columnNames[COL_BOUGHT_AMT] = "Rupees";
+		columnNames[COL_NET_QTY] = "Qty";
+		columnNames[COL_NET_AMT] = "Rupees";
 	}
 
 	/**
@@ -83,50 +90,70 @@ public class ItemsTable extends JTable {
 			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
 				JLabel label = null;
 
+				StringBuilder sb;
+				String netColor;
 				ItemGroup group = (ItemGroup) value;
 				switch (col) {
 				case COL_ITEMNAME:
 					ImageIcon img = ImageManager.getItemImage(group.getItem());
 					label = new JLabel(group.getItem(), img, SwingConstants.LEFT);
 					break;
-				case COL_SOLD:
+				case COL_SOLD_QTY:
 					if (group.getSoldQuantity() == 0) {
-						label = new JLabel("-");
+						label = new JLabel("-", SwingConstants.CENTER);
 					} else {
-						label = new JLabel(nf.format(group.getSoldQuantity()) + " / " + nf.format(group.getSoldAmount()) + "r");
+						label = new JLabel(formatQuantity(group.getSoldQuantity()));
 					}
 					break;
-				case COL_BOUGHT:
+				case COL_SOLD_AMT:
+					if (group.getSoldQuantity() == 0) {
+						label = new JLabel("-", SwingConstants.CENTER);
+					} else {
+						label = new JLabel(formatRupees(group.getSoldAmount()));
+					}
+					break;
+				case COL_BOUGHT_QTY:
 					if (group.getBoughtQuantity() == 0) {
-						label = new JLabel("-");
+						label = new JLabel("-", SwingConstants.CENTER);
 					} else {
-						label = new JLabel(nf.format(group.getBoughtQuantity()) + " / " + nf.format(group.getBoughtAmount()) + "r");
+						label = new JLabel(formatQuantity(group.getBoughtQuantity()));
 					}
 					break;
-				case COL_NET:
-					StringBuilder sb = new StringBuilder();
+				case COL_BOUGHT_AMT:
+					if (group.getBoughtQuantity() == 0) {
+						label = new JLabel("-", SwingConstants.CENTER);
+					} else {
+						label = new JLabel(formatRupees(group.getBoughtAmount()));
+					}
+					break;
+				case COL_NET_QTY:
+					sb = new StringBuilder();
 					sb.append("<html>");
 
-					if (group.getNetQuantity() < 0) {
-						sb.append("<font color=red>");
-						sb.append(nf.format(group.getNetQuantity()));
+					netColor = getNetColor(group.getNetQuantity());
+					if (netColor != null) {
+						sb.append("<font color=").append(netColor).append(">");
+						sb.append(formatQuantity(group.getNetQuantity()));
 						sb.append("</font>");
 					} else {
-						sb.append("<font color=green>");
-						sb.append('+').append(nf.format(group.getNetQuantity()));
-						sb.append("</font>");
+						sb.append(formatQuantity(group.getNetQuantity()));
 					}
 
-					sb.append(" / ");
+					sb.append("</html>");
 
-					if (group.getNetAmount() < 0) {
-						sb.append("<font color=red>");
-						sb.append(nf.format(group.getNetAmount())).append('r');
+					label = new JLabel(sb.toString());
+					break;
+				case COL_NET_AMT:
+					sb = new StringBuilder();
+					sb.append("<html>");
+
+					netColor = getNetColor(group.getNetAmount());
+					if (netColor != null) {
+						sb.append("<font color=").append(netColor).append(">");
+						sb.append(formatRupees(group.getNetAmount()));
 						sb.append("</font>");
 					} else {
-						sb.append("<font color=green>");
-						sb.append('+').append(nf.format(group.getNetAmount())).append('r');
-						sb.append("</font>");
+						sb.append(formatRupees(group.getNetAmount()));
 					}
 
 					sb.append("</html>");
@@ -142,6 +169,69 @@ public class ItemsTable extends JTable {
 
 				return label;
 			}
+
+			/**
+			 * Gets the font color to use for the "net" column.
+			 * @param number the number (e.g. rupees or quantity)
+			 * @return the color or null if the number is zero
+			 */
+			private String getNetColor(int number) {
+				if (number < 0) {
+					return "red";
+				}
+				if (number > 0) {
+					return "green";
+				}
+				return null;
+			}
+
+			/**
+			 * Formats a rupee amount as a string.
+			 * @param rupees the amount of rupees
+			 * @return the rupee string
+			 */
+			private String formatRupees(int rupees) {
+				return formatQuantity(rupees) + "r";
+			}
+
+			/**
+			 * Formats a quantity as a string
+			 * @param quantity the quantity
+			 * @return the quantity string
+			 */
+			private String formatQuantity(int quantity) {
+				StringBuilder sb = new StringBuilder();
+				if (quantity > 0) {
+					sb.append('+');
+				}
+				sb.append(nf.format(quantity));
+				return sb.toString();
+			}
 		});
+
+		//set the width of "item name" column so the name isn't snipped
+		columnModel.getColumn(COL_ITEMNAME).setMinWidth(200);
+
+		//set the column groupings
+		{
+			List<ColumnGroup> columnGroups = new ArrayList<ColumnGroup>();
+
+			ColumnGroup columnGroup = new ColumnGroup("Sold");
+			columnGroup.add(columnModel.getColumn(COL_SOLD_QTY));
+			columnGroup.add(columnModel.getColumn(COL_SOLD_AMT));
+			columnGroups.add(columnGroup);
+
+			columnGroup = new ColumnGroup("Bought");
+			columnGroup.add(columnModel.getColumn(COL_BOUGHT_QTY));
+			columnGroup.add(columnModel.getColumn(COL_BOUGHT_AMT));
+			columnGroups.add(columnGroup);
+
+			columnGroup = new ColumnGroup("Net");
+			columnGroup.add(columnModel.getColumn(COL_NET_QTY));
+			columnGroup.add(columnModel.getColumn(COL_NET_AMT));
+			columnGroups.add(columnGroup);
+
+			setColumnGroups(columnGroups);
+		}
 	}
 }
