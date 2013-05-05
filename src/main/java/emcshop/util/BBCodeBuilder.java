33f5245c14b66e5e3e406a55a2981f5e67bfede6
Mike Angstadt <mike.angstadt@gmail.com>
2013-05-04 20:32:26 -0400

    Added Javadocs.

--- src/main/java/emcshop/util/BBCodeBuilder.java
+++ src/main/java/emcshop/util/BBCodeBuilder.java
@@ -1,12 +1,3 @@
-/*
- * Copyright(c) 2012 Siemens Medical Solutions Health
- * Services Corporation.  All rights reserved.  This software is
- * confidential, proprietary to Siemens, is protected by
- * copyright laws in the U.S. and abroad, and is licensed for use
- * by customers only in strict accordance with the license
- * agreement governing its use.
- */
-
 package emcshop.util;
 
 import java.util.Stack;
@@ -16,55 +7,111 @@ import java.util.Stack;
  * @author Michael Angstadt
  */
 public class BBCodeBuilder {
+	private static final String NEWLINE = System.getProperty("line.separator");
 	private final StringBuilder bbCode = new StringBuilder();
 	private final Stack<String> openTags = new Stack<String>();
 
+	/**
+	 * Opens an underline tag.
+	 * @return this
+	 */
 	public BBCodeBuilder u() {
 		return open("u");
 	}
 
+	/**
+	 * Appends underlined text.
+	 * @param text the text to append
+	 * @return this
+	 */
 	public BBCodeBuilder u(String text) {
 		return u().text(text).close();
 	}
 
+	/**
+	 * Opens a strike-through tag.
+	 * @return this
+	 */
 	public BBCodeBuilder s() {
 		return open("s");
 	}
 
+	/**
+	 * Appends strike-through text.
+	 * @param text the text to append
+	 * @return this
+	 */
 	public BBCodeBuilder s(String text) {
 		return s().text(text).close();
 	}
 
+	/**
+	 * Opens a bold tag.
+	 * @return this
+	 */
 	public BBCodeBuilder b() {
 		return open("b");
 	}
 
+	/**
+	 * Appends bolded text.
+	 * @param text the text to append
+	 * @return this
+	 */
 	public BBCodeBuilder b(String text) {
 		return b().text(text).close();
 	}
 
+	/**
+	 * Opens a font tag.
+	 * @param font the font (e.g. "courier new")
+	 * @return this
+	 */
 	public BBCodeBuilder font(String font) {
 		return open("font", font);
 	}
 
+	/**
+	 * Appends text with a certain font.
+	 * @param font the font
+	 * @param text the text to append
+	 * @return this
+	 */
 	public BBCodeBuilder font(String font, String text) {
 		return font(font).text(text).close();
 	}
 
+	/**
+	 * Opens a color tag.
+	 * @param color the color (e.g. "green")
+	 * @return this
+	 */
 	public BBCodeBuilder color(String color) {
 		return open("color", color);
 	}
 
+	/**
+	 * Appends colored text.
+	 * @param color the color (e.g. "green")
+	 * @param text the text to append
+	 * @return this
+	 */
 	public BBCodeBuilder color(String color, String text) {
 		return color(color).text(text).close();
 	}
 
+	/**
+	 * Appends a URL.
+	 * @param url the URL (e.g. "http://example.com")
+	 * @return this
+	 */
 	public BBCodeBuilder url(String url) {
 		return open("url").text(url).close();
 	}
 
 	/**
-	 * @param url the URL
+	 * Appends a URL.
+	 * @param url the URL (e.g. "http://example.com")
 	 * @param text the display text
 	 * @return
 	 */
@@ -77,13 +124,24 @@ public class BBCodeBuilder {
 	 * @return this
 	 */
 	public BBCodeBuilder nl() {
-		return text(System.getProperty("line.separator"));
+		return text(NEWLINE);
 	}
 
+	/**
+	 * Opens a tag.
+	 * @param tagName the tag name (e.g. "b")
+	 * @return this
+	 */
 	public BBCodeBuilder open(String tagName) {
 		return open(tagName, null);
 	}
 
+	/**
+	 * Opens a tag.
+	 * @param tagName the tag name (e.g. "color")
+	 * @param tagValue the tag value (e.g. "green")
+	 * @return this
+	 */
 	public BBCodeBuilder open(String tagName, String tagValue) {
 		bbCode.append('[').append(tagName);
 		if (tagValue != null) {
@@ -94,11 +152,21 @@ public class BBCodeBuilder {
 		return this;
 	}
 
+	/**
+	 * Appends text.
+	 * @param text the text to append
+	 * @return this
+	 */
 	public BBCodeBuilder text(CharSequence text) {
 		bbCode.append(text.toString());
 		return this;
 	}
 
+	/**
+	 * Appends a character.
+	 * @param ch the character to append
+	 * @return this
+	 */
 	public BBCodeBuilder text(char ch) {
 		bbCode.append(ch);
 		return this;
@@ -112,7 +180,7 @@ public class BBCodeBuilder {
 	public BBCodeBuilder close() {
 		if (!openTags.isEmpty()) {
 			String tag = openTags.pop();
-			bbCode.append("[/").append(tag).append(']');
+			closeTag(tag, bbCode);
 		}
 		return this;
 	}
@@ -120,25 +188,33 @@ public class BBCodeBuilder {
 	/**
 	 * Closes the last X number of tags that were opened. This has no effect if
 	 * there are no open tags.
-	 * @param numberOfTagsToClose the number of tags to close
+	 * @param tagsToClose the number of tags to close
 	 * @return this
 	 */
-	public BBCodeBuilder close(int numberOfTagsToClose) {
-		for (int i = 0; i < numberOfTagsToClose; i++) {
+	public BBCodeBuilder close(int tagsToClose) {
+		for (int i = 0; i < tagsToClose; i++) {
 			close();
 		}
 		return this;
 	}
 
+	/**
+	 * Generates the BBCode string. All open tags are automatically closed.
+	 * @return the BBCode string
+	 */
 	@Override
 	public String toString() {
 		//close all open tags without modifying the buffer
 		StringBuilder sb = new StringBuilder(bbCode);
 		for (int i = openTags.size() - 1; i >= 0; i--) {
 			String tag = openTags.get(i);
-			sb.append("[/").append(tag).append(']');
+			closeTag(tag, sb);
 		}
 
 		return sb.toString();
+	}
+
+	private void closeTag(String tag, StringBuilder sb) {
+		sb.append("[/").append(tag).append(']');
 	}
 }
