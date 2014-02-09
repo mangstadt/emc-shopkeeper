fd78d76c06f1a2a296d4b469448f04a1e86c702a
Mike Angstadt <mike.angstadt@gmail.com>
2014-02-09 18:50:09 -0500

    Javadoc tweaks.

--- src/main/java/emcshop/EMCShopkeeper.java
+++ src/main/java/emcshop/EMCShopkeeper.java
@@ -2,10 +2,8 @@ package emcshop;
 
 /**
  * Alias for the {@link Main} class so that "EMCShopkeeper" instead of "Main"
- * appears on the Mac menu bar. The {@link Main} class wasn't renamed so people
- * don't have to download the JNLP file again.
- * @author mangst
- * 
+ * appears in the Mac menu bar. The {@link Main} class wasn't renamed because
+ * that would require updating the JNLP file.
  */
 public class EMCShopkeeper {
 	public static void main(String args[]) throws Throwable {
