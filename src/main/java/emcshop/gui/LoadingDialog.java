6066b1962d1ae649b270a020e058f7ac0a55f2ec
Mike Angstadt <mike.angstadt@gmail.com>
2014-02-15 15:20:16 -0500

    Cleaned up LoadingDialog code.

--- src/main/java/emcshop/gui/LoadingDialog.java
+++ src/main/java/emcshop/gui/LoadingDialog.java
@@ -1,28 +1,23 @@
 package emcshop.gui;
 
-import java.awt.FlowLayout;
-
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JRootPane;
 
+import net.miginfocom.swing.MigLayout;
 import emcshop.gui.images.ImageManager;
 
 @SuppressWarnings("serial")
 public class LoadingDialog extends JDialog {
-	public LoadingDialog(final MainFrame owner, String title, String text) {
+	public LoadingDialog(MainFrame owner, String title, String text) {
 		super(owner, title, true);
 		setResizable(false);
 		setUndecorated(true);
 		getRootPane().setWindowDecorationStyle(JRootPane.NONE);
 
-		setLayout(new FlowLayout());
-
-		JPanel p = new JPanel(new FlowLayout());
-		p.add(new JLabel(ImageManager.getLoading()));
-		p.add(new JLabel("<html><b>" + text + "</b></html>"));
-		add(p, "align center");
+		setLayout(new MigLayout());
+		add(new JLabel(ImageManager.getLoading()), "split 2, align center");
+		add(new JLabel("<html><b>" + text + "</b></html>"));
 
 		pack();
 		setLocationRelativeTo(owner);
