fd6f2e838f81966256d25c51832ee2b7f6cae1d5
Michael Angstadt <mike.angstadt@gmail.com>
2014-04-15 10:48:14 -0400

    Fleshed out the case-insensitive collection classes.

--- src/main/java/emcshop/util/CaseInsensitiveHashSet.java
+++ src/main/java/emcshop/util/CaseInsensitiveHashSet.java
@@ -1,10 +1,16 @@
 package emcshop.util;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 import com.google.common.collect.ForwardingSet;
 
+/**
+ * A set of strings where case is ignored (all values are converted to lower
+ * case).
+ */
 public class CaseInsensitiveHashSet extends ForwardingSet<String> {
 	private final Set<String> set = new HashSet<String>();
 
@@ -14,17 +20,70 @@ public class CaseInsensitiveHashSet extends ForwardingSet<String> {
 
 	@Override
 	public boolean contains(Object value) {
-		String s = (String) value;
-		return set.contains(s.toLowerCase());
+		return super.contains(sanitize(value));
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> values) {
+		return super.containsAll(sanitize(values));
 	}
 
 	@Override
 	public boolean add(String value) {
-		return set.add(value.toLowerCase());
+		return super.add(sanitize(value));
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends String> values) {
+		return super.addAll(sanitizeStr(values));
+	}
+
+	@Override
+	public boolean remove(Object value) {
+		return super.remove(sanitize(value));
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> values) {
+		return super.removeAll(sanitize(values));
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> values) {
+		return super.retainAll(sanitize(values));
 	}
 
 	@Override
 	protected Set<String> delegate() {
 		return set;
+	}
+
+	private String sanitize(String value) {
+		return value.toLowerCase();
+	}
+
+	private Object sanitize(Object value) {
+		if (value instanceof String) {
+			return sanitize((String) value);
+		}
+		return value;
+	}
+
+	private Collection<Object> sanitize(Collection<?> values) {
+		Collection<Object> sanitized = new ArrayList<Object>(values.size());
+		for (Object value : values) {
+			Object s = sanitize(value);
+			sanitized.add(s);
+		}
+		return sanitized;
+	}
+
+	private Collection<String> sanitizeStr(Collection<? extends String> values) {
+		Collection<String> sanitized = new ArrayList<String>(values.size());
+		for (String value : values) {
+			String s = sanitize(value);
+			sanitized.add(s);
+		}
+		return sanitized;
 	}
 }
--- src/main/java/emcshop/util/CaseInsensitiveMultimap.java
+++ src/main/java/emcshop/util/CaseInsensitiveMultimap.java
@@ -1,11 +1,18 @@
 package emcshop.util;
 
 import java.util.List;
+import java.util.Map;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ForwardingListMultimap;
 import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimap;
 
+/**
+ * A multimap that has case-insensitive strings as its key (they are converted
+ * to lower case).
+ * @param <V> the value class
+ */
 public class CaseInsensitiveMultimap<V> extends ForwardingListMultimap<String, V> {
 	private final ListMultimap<String, V> map = ArrayListMultimap.create();
 
@@ -15,28 +22,71 @@ public class CaseInsensitiveMultimap<V> extends ForwardingListMultimap<String, V
 
 	@Override
 	public List<V> get(String key) {
-		return map.get(key.toLowerCase());
+		return super.get(sanitize(key));
 	}
 
 	@Override
 	public boolean put(String key, V value) {
-		return map.put(key.toLowerCase(), value);
+		return super.put(sanitize(key), value);
+	}
+
+	@Override
+	public boolean putAll(Multimap<? extends String, ? extends V> values) {
+		boolean changed = false;
+		for (Map.Entry<? extends String, ? extends V> entry : values.entries()) {
+			String key = entry.getKey();
+			V value = entry.getValue();
+
+			if (put(key, value)) {
+				changed = true;
+			}
+		}
+		return changed;
+	}
+
+	@Override
+	public boolean putAll(String key, Iterable<? extends V> values) {
+		return super.putAll(sanitize(key), values);
+	}
+
+	@Override
+	public boolean remove(Object key, Object value) {
+		return super.remove(sanitize(key), value);
 	}
 
 	@Override
 	public List<V> removeAll(Object key) {
-		String s = (String) key;
-		return map.removeAll(s.toLowerCase());
+		return super.removeAll(sanitize(key));
+	}
+
+	@Override
+	public List<V> replaceValues(String key, Iterable<? extends V> values) {
+		return super.replaceValues(sanitize(key), values);
+	}
+
+	@Override
+	public boolean containsEntry(Object key, Object value) {
+		return super.containsEntry(sanitize(key), value);
 	}
 
 	@Override
 	public boolean containsKey(Object key) {
-		String s = (String) key;
-		return map.containsKey(s.toLowerCase());
+		return super.containsKey(sanitize(key));
 	}
 
 	@Override
 	protected ListMultimap<String, V> delegate() {
 		return map;
+	}
+
+	private String sanitize(String value) {
+		return value.toLowerCase();
+	}
+
+	private Object sanitize(Object value) {
+		if (value instanceof String) {
+			return sanitize((String) value);
+		}
+		return value;
 	}
 }
