7954642afae750dd0dddb5471536a879487e021a
Michael Angstadt <mike.angstadt@gmail.com>
2015-07-19 10:06:08 -0400

    Initializes size of Listeners to 1.

--- src/main/java/emcshop/util/Listeners.java
+++ src/main/java/emcshop/util/Listeners.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class Listeners extends ArrayList<ActionListener> {
+	public Listeners() {
+		super(1);
+	}
+
 	public void fire() {
 		for (ActionListener listener : this) {
 			listener.actionPerformed(null);
