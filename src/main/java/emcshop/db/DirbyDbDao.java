7fcd20024b5b92c52bf3695634f8ce1cd9bf197a
Mike Angstadt <mike.angstadt@gmail.com>
2017-05-28 16:24:32 -0400

    Add mule and donkey egg item names

--- src/main/java/emcshop/db/DirbyDbDao.java
+++ src/main/java/emcshop/db/DirbyDbDao.java
@@ -58,7 +58,7 @@ public abstract class DirbyDbDao implements DbDao {
 	 * directly. Use {@link #getAppDbVersion()} instead, because this method
 	 * gets overridden in unit tests.
 	 */
-	public static final int schemaVersion = 35;
+	public static final int schemaVersion = 36;
 
 	protected Connection conn;
 	protected String jdbcUrl;
