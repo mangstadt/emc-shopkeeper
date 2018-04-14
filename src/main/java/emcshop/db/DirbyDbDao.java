267d9fac137cb60dae0cd9e9f32b91d583b6e970
Mike Angstadt <mike.angstadt@gmail.com>
2018-04-14 12:40:29 -0400

    Update items list

--- src/main/java/emcshop/db/DirbyDbDao.java
+++ src/main/java/emcshop/db/DirbyDbDao.java
@@ -58,7 +58,7 @@ public abstract class DirbyDbDao implements DbDao {
 	 * directly. Use {@link #getAppDbVersion()} instead, because this method
 	 * gets overridden in unit tests.
 	 */
-	public static final int schemaVersion = 38;
+	public static final int schemaVersion = 39;
 
 	protected Connection conn;
 	protected String jdbcUrl;
