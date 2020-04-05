b26b039755772e37d382016566adcbf2611ee269
Michael Angstadt <mike.angstadt@gmail.com>
2020-04-05 14:17:21 -0400

    Create SQL migration script for item name changes

--- src/main/java/emcshop/db/DirbyDbDao.java
+++ src/main/java/emcshop/db/DirbyDbDao.java
@@ -58,7 +58,7 @@ public abstract class DirbyDbDao implements DbDao {
 	 * directly. Use {@link #getAppDbVersion()} instead, because this method
 	 * gets overridden in unit tests.
 	 */
-	public static final int schemaVersion = 40;
+	public static final int schemaVersion = 41;
 
 	protected Connection conn;
 	protected String jdbcUrl;
