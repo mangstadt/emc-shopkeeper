81c1efcfbe6ef14e20d29e92bd1c379095951864
Michael Angstadt <mike.angstadt@gmail.com>
2020-12-20 14:39:19 -0500

    Tweak dialog wording

--- src/main/java/emcshop/gui/MainFrame.java
+++ src/main/java/emcshop/gui/MainFrame.java
@@ -310,16 +310,19 @@ public class MainFrame extends JFrame {
 					String answer = DialogBuilder.question() //@formatter:off
 						.title("Download Threads")
 						.text("THIS SETTING IS FOR POWER USERS ONLY! :)",
-							"How many threads do you want to use for downloading and parsing rupee transactions?",
+							"This screen allows you to define the number threads you want to use for downloading",
+							"and parsing rupee transactions.",
 							"",
 							"Each thread is responsible for downloading a rupee history transaction page and then",
 							"extracting the transactions out of the page. Another thread (not included in this count)",
 							"is responsible for inserting the transactions into the EMC Shopkeeper database.",
 							"",
 							"Your computer has " + cores + " logical cores. It is recommended that this setting be set to the number",
-							"of logic cores you computer has, or 4 if your computer has less than 4 cores.",
+							"of logical cores you computer has, or 4 if your computer has less than 4 cores.",
+							"",
 							//using the string "recommended" allows the setting to be transferable between computers
-							"Type \"" + valueForRecommended + "\" to use the recommended value.")
+							"Type \"" + valueForRecommended + "\" to use the recommended setting, or the number of threads you",
+							"specifically want to use.")
 						.showInput(textboxValue); //@formatter:on
 
 					boolean dialogWasCanceled = (answer == null);
