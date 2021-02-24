299d954648377fbda345e61a15a5dfeeadb10703
Michael Angstadt <mike.angstadt@gmail.com>
2021-02-24 11:53:24 -0500

    Fix issue where runtime exception is thrown on startup
    Caused by duplicate item name in ITEMS database table.
    
    https://github.com/mangstadt/emc-shopkeeper/issues/41

--- src/main/java/emcshop/db/DirbyDbDao.java
+++ src/main/java/emcshop/db/DirbyDbDao.java
@@ -61,7 +61,7 @@ public abstract class DirbyDbDao implements DbDao {
 	 * directly. Use {@link #getAppDbVersion()} instead, because this method
 	 * gets overridden in unit tests.
 	 */
-	public static final int schemaVersion = 49;
+	public static final int schemaVersion = 50;
 
 	protected Connection conn;
 	protected String jdbcUrl;
